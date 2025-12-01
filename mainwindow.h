5fc46da0b00344d976528ed0e56678cef0a26ae6
kihochalong <41330247@nfu.edu.tw>
2025-12-01 18:07:37 +0800

    Deploy Project

---
+++ mainwindow.h
@@ -0,0 +1,19 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "ui_mainwindow.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;  // ← 這裡不是 public 繼承，而是 pointer
+};
+
+#endif // MAINWINDOW_H
